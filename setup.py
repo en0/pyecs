500e241b4cd647c520faff85225238c8c3875b4a
Ian Laird <irlaird@gmail.com>
2022-04-04 00:38:25 -0600

    Bumped Version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", "r") as fp:
 
 setup(
     name="pyecs",
-    version="1.0.0",
+    version="1.0.1",
     description="A pygame framwork",
     long_description=long_description,
     long_description_content_type="text/markdown",
